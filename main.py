9b3c2fc58680744332de1a6096226c4cd7a44ed4
russellchao <russchaoman@gmail.com>
2025-05-08 18:50:24 -0400

    Created function to generate OpenAI response

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-user_prompt = input("Enter anything here: ")
+from apiKeyInfo import * # contains hidden API keys
+import openai
 
-print(user_prompt)
+
+def generate_openai(user_prompt):
+
+    client = openai.OpenAI(api_key=openaiApiKey)
+    
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini", 
+        messages = [
+            {"role": "system", "content": "You are a master at answering anything."},
+            {"role": "user", "content": user_prompt},
+        ]
+    )
+
+    return response.choices[0].message.content
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    user_prompt = input("Enter anything here: ")
+
+    print("OpenAI Response: ", generate_openai(user_prompt))
+
